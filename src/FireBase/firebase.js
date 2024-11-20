1fb32f9d7db1a06fa5ea13fe95932f5e82c65c9b
Abdullah <abdullahvora136@gmail.com>
2024-11-20 10:45:48 +0530

    set up env

--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCVRr7eq9YOUghAGDgUwV57GtaWyNff388",
-  authDomain: "fir-1-3f6df.firebaseapp.com",
-  databaseURL: "https://fir-1-3f6df-default-rtdb.firebaseio.com",
-  projectId: "fir-1-3f6df",
-  storageBucket: "fir-1-3f6df.appspot.com",
-  messagingSenderId: "1094341646587",
-  appId: "1:1094341646587:web:16087628144d605634785e",
-  measurementId: "G-Y0N32H33Y3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
